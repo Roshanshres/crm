89989d668618f7cee678443ea81d8c872e074c26
Roshanshres <roshanshrestha992@gmail.com>
2023-10-03 23:08:41 +0545

    home prodcu and customer fucntion create and sent to accout url

--- accounts/views.py
+++ accounts/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 
 # Create your views here. functions and classes
 
+from django.http import HttpResponse 
 
+
+def home(request):
+    return HttpResponse('Home Page')
+
+def products(request):
+    return HttpResponse('products')
+
+
+def customer(request):
+    return HttpResponse('customer')
